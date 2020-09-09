a3131340174ca2dd7337e4d7507e3f48b3de6c7e
Enrique Lopez <elop22@gmail.com>
2020-09-09 15:56:59 -0600

    moved connection

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO department(name) VALUES (“accouting”), (“sales”), (“engineering”), (“legal”);
-INSERT INTO role(title, salary, department_id) VALUES (“Accountant”, 95000, 1), (“Account Manager”, 65000, 1), (“Sales Rep”, 75000, 2), (“Sales Lead”, 125000, 2), (“Junior Developer”, 48000, 3), (“Senior Software Developer”, 175000, 3 ), (“Legal Assistant”, 35000, 4), (“Lawyer”, 175000, 4);
-INSERT INTO employee(first_name, last_name, role_id) VALUES (“Fred”, “Willard”, 1), (“Catherine”, “O’Hara”, 2), (“Christopher”, “Guest”, 3), (“Parker”, “Posey”,4)
+INSERT INTO department(department_name) VALUES (“accouting”), (“sales”), (“engineering”), (“legal”);
+INSERT INTO role(title, salary, department_id) VALUES (“Accountant”, 95000, 1), (“Account Manager”, 65000, 1), (“Sales Rep”, 75000, 2), (“Sales Lead”, 125000, 2), (“Junior Developer”, 65000, 3), (“Senior Software Developer”, 175000, 3 ), (“Legal Assistant”, 45000, 4), (“Lawyer”, 175000, 4);
+INSERT INTO employee(first_name, last_name, role_id) VALUES (“George”, “Williams”, 1), (“Katy”, “O’Connell”, 2), (“Kristoff”, “Points”, 3), (“Parker”, “James”,4)
