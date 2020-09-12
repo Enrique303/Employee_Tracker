01c9d349b71b288ddff1f0312af1e08badaf3431
Enrique Lopez <elop22@gmail.com>
2020-09-12 12:27:07 -0600

    added prompt and switch cases

--- app.js
+++ app.js
@@ -3,3 +3,48 @@ const inquirer = require('inquirer');
 const Employee = require('./employee');
 const Role = require('./role');
 
+function mainMenu() {
+  inquirer
+    .prompt({
+      name: "mainMenu",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employess",
+        "View All Employess By Department",
+        "view All Employess By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "exit"
+      ]
+    })
+    .then(function(answer){
+      switch (answer.action){
+      case "View All Employess":
+        viewAllEmployees()
+        break;
+      case "View All Employess By Department":
+        viewByDepartment()
+        break;
+      case "view All Employess By Manager":
+        viewByManager()
+        break;
+      case "Add Employee":
+        addEmployee()
+        break;
+      case "Remove Employee":
+        removeEmployee()
+        break;
+      case "Update Employee Role":
+        updateEmployeeRole()
+        break;
+      case "Update Employee Manager":
+        updateEmployeeManager()
+        break;
+      default:
+        exit();
+      };
+    })
+}
