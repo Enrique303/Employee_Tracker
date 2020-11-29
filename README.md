0beff98203382a9482df12dbe2d9ebc22866d8f7
Enrique Lopez <elop22@gmail.com>
2020-11-28 19:32:39 -0700

    fixed

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 ## Description 
 This app is allows users to track employees, their salaries, roles and department.  The app is using MYSQL and users can add update and delete employees.
+
+## Installation
+You can install by forking the repository. You will need to load npm packages for MySQL, Inquirer, and console-table-printer with command "npm i.
+
+## Usage
+Once loaded, boot the application with "node server". From there the prompts will guide you to add employees. MySQL schema and seed files are included in the repository. Simply copy the contents of these files and execute them in MySQL workbench.
+## Credits
+I produced this application using the exercises taught in class.
+
+## License
+This application is ISC licensed
+
+## Contribution
+Please contact me with the information in the contact section to make suggestions.
+
+## Questions/Contact
+Contact me:
+
+Email: elop22@gmail.com;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require('inquirer');
-const  printTable  = require('console-table-printer');
+const  { printTable }  = require('console-table-printer');
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 
   user: "root",
 
-  password: "Mclane11-$",
+  password: "",
   database: "employeeDB"
 });
 
@@ -37,24 +37,18 @@ function mainMenu() {
       ]
     })
     .then(function(answer){
-      switch (answer.action){
+      switch (answer.mainMenu){
       case "View All Employess":
         viewAllEmployees()
         break;
       case "View All Employess By Department":
         viewByDepartment()
         break;
-      case "view All Employess By Manager":
-        viewByManager()
-        break;
       case "Add Employee":
         addEmployee()
         break;
       case "Remove Employee":
         removeEmployee()
-        break;
-      case "Update Employee Role":
-        updateEmployeeRole()
         break;
       case "Update Employee Manager":
         updateEmployeeManager()
@@ -67,8 +61,7 @@ function mainMenu() {
 };
 
 function viewAllEmployees() {
-  const query = "SELECT * FROM employee";
-  connection.query(query, function(err, res) {
+  connection.query("SELECT * FROM employee", function(err, res) {
     if (err)throw err;
     printTable(res);
     mainMenu()
@@ -94,7 +87,102 @@ function viewByDepartment() {
     )
   }).then(function(answer){
     connection.query("SELECT employee.id, employee.first_name, employee.last_name, FROM employee JOIN")
+    printTable(res);
   })
+  mainMenu();
 };
+const addEmployee = () => {
+  connection.query("SELECT * FROM role_", (err, res) => {
+    if (err) throw err
+
+    return inquirer.prompt([
+
+      {
+        type: "input",
+        name: "firstName",
+        message: "Please enter the employee's first name:",
+
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Please enter the employee's last name:"
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "What role is this employee filling?",
+        choices: () => {
+          let roleArray = [];
+          for (let i = 0; i < res.length; i++) {
+            roleArray.push(res[i].title + " | " + res[i].id);
+          }
+          return roleArray;
+        }
+      }
+    ]).then((answer) => {
+      let roleID = answer.role.split("|")[1];
+      connection.query("SELECT * FROM employee", (err, res) => {
+        inquirer.prompt([
+          {
+            type: "list",
+            name: "manager",
+            message: "Who will this employee report to?",
+            choices: () => {
+              let managerArray = [];
+              for (let i = 0; i < res.length; i++) {
+                managerArray.push(res[i].first_name + res[i].last_name + " | " + res[i].id);
+              }
+              console.log()
+              return managerArray;
+            }
+          }
+
+        ]).then((choice) => {
+          let managerID = choice.manager.split("|")[1];
+          connection.query(`INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("${answer.firstName}", "${answer.lastName}", ${roleID}, ${managerID}) `, (err, res) => {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            console.log(`${answer.firstName} ${answer.lastName}  has been added to the team! Welcome Aboard!`);
+          })
+          console.log("");
+          mainMenu();
+        })
+      })
+    })
+  })
+}
+const removeEmployee = () => {
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "name",
+        message: "What employee record are you deleting?",
+        choices: () => {
+          let employeeArray = [];
+          for (let i = 0; i < res.length; i++) {
+            employeeArray.push(res[i].first_name + " " + res[i].last_name + " | " + res[i].id);
+          }
+          return employeeArray;
+        }
+      },
+
+
+    ]).then((choice) => {
+
+      let employeeID = choice.name.split("|")[1];
+      connection.query(
+        `DELETE FROM employee WHERE employee.id = ${employeeID} `,
+        (err) => {
+          if (err) throw err;
+          mainMenu();
+        }
+      )
+    })
+    
+  });
+}
 
 // mainMenu();
